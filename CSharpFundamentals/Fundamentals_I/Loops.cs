3780818cbe6546f6b1c3adb0991b327944a2bf60
BinaLati5701 <79129945+BinaLati5701@users.noreply.github.com>
2023-02-19 00:00:58 -0600

    Add files via upload
    added loops

---
+++ CSharpFundamentals/Fundamentals_I/Loops.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFundamentals.Fundamentals_I
+{
+    public class Loops
+    {
+        public void PrintValues()
+        {
+            for (int i = 1; i < 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public void DivisibleByThreeOrFive()
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+        }
+
+        public void FizzBuzzWithWhileLoop()
+        {
+            int i = 1;
+            while(i < 100)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                
+                i++;
+            }
+           
+
+        }
+    }
+}
---
+++ CSharpFundamentals/Program.cs
@@ -0,0 +1,18 @@
+﻿using CSharpFundamentals.Fundamentals_I;
+using System;
+
+namespace CSharpFundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            Loops loops = new Loops();
+            loops.PrintValues();
+            loops.DivisibleByThreeOrFive();
+            loops.FizzBuzzWithWhileLoop();
+        }
+    }
+}
